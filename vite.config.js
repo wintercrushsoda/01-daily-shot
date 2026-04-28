3b8c7b154f959ebdfba3ad87016da764310c1e44
DESKTOP-ARURP4U\juyeon <jy_jo@robocare.co.kr>
2026-04-28 16:16:06 +0900

    Fix Netlify asset base path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/01-daily-shot/',
   plugins: [
     vue(),
     vueDevTools(),
